197271139a7cce1cb2508f2033791c419c50f93c
Jason Ward <jason.ward@policystat.com>
2014-04-11 14:45:51 -0400

    refs #1: Built the base framework for how I envision this guy working.

--- html2docx/core.py
+++ html2docx/core.py
@@ -1,7 +1,30 @@
 class HTML2Docx(object):
     def __init__(self, html, out_file):
+        """
+        ``html`` is the raw HTML that we want to convert to a docx.
+        ``out_file`` After execution, ``out_file`` will be the docx in
+        question.
+        """
         self.html = html
         self.out_file = out_file
 
-    def convert():
+    def convert(self):
+        """
+        This is the entry point for this guy.
+        """
+        self._convert()
+        self._write_docx()
+
+    def _convert(self):
+        """
+        Called by ``convert`` to get build the contents of the docx, returns
+        nothing.
+        """
+        pass
+
+    def _write_docx(self):
+        """
+        Called by ``convert`` to actually build the real docx file, returns
+        nothing.
+        """
         pass
