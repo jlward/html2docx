ab67017e7ce17bf566cc7e618571fe2a08afef02
Jason Ward <jason.ward@policystat.com>
2014-04-13 13:11:10 -0400

    res #3: Table test cases.

--- html2docx/tests/test_builder.py
+++ html2docx/tests/test_builder.py
@@ -5,8 +5,10 @@ from html2docx.builder import (
     Paragraph,
     ParagraphParser,
     RunProperties,
+    Table,
     TableCell,
     TableCellParser,
+    TableParser,
     TableRow,
     TableRowParser,
 )
@@ -142,6 +144,41 @@ class TableRowTestCase(TestCase):
     def test_empty(self):
         table_row = TableRow()
         expected_xml = '<w:tr />'
+
+        xml = table_row.xml
+        self.assertEqual(xml, expected_xml)
+
+
+class TableParserTestCase(TestCase):
+    def test_simple(self):
+        element = cElementTree.fromstring('<table><tr><td>AAA</td></tr></table>')  # noqa
+        parser = TableParser(element)
+        xml = parser.tag.xml
+        expected_xml = '<w:tbl><w:tr><w:tc><w:p><w:r><w:rPr /><w:t>AAA</w:t></w:r></w:p></w:tc></w:tr></w:tbl>'  # noqa
+
+        self.assertEqual(xml, expected_xml)
+
+    def test_with_style(self):
+        element = cElementTree.fromstring('<table><tr><td><strong>AAA</strong></td></tr></table>')  # noqa
+        parser = TableParser(element)
+        xml = parser.tag.xml
+        expected_xml = '<w:tbl><w:tr><w:tc><w:p><w:r><w:rPr><w:b /></w:rPr><w:t>AAA</w:t></w:r></w:p></w:tc></w:tr></w:tbl>'  # noqa
+
+        self.assertEqual(xml, expected_xml)
+
+    def test_multiple_cells(self):
+        element = cElementTree.fromstring('<table><tr><td>AAA</td><td>BBB</td></tr><tr><td>CCC</td><td>DDD</td></tr></table>')  # noqa
+        parser = TableParser(element)
+        xml = parser.tag.xml
+        expected_xml = '<w:tbl><w:tr><w:tc><w:p><w:r><w:rPr /><w:t>AAA</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr /><w:t>BBB</w:t></w:r></w:p></w:tc></w:tr><w:tr><w:tc><w:p><w:r><w:rPr /><w:t>CCC</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr /><w:t>DDD</w:t></w:r></w:p></w:tc></w:tr></w:tbl>'  # noqa
+
+        self.assertEqual(xml, expected_xml)
+
+
+class TableTestCase(TestCase):
+    def test_empty(self):
+        table_row = Table()
+        expected_xml = '<w:tbl />'
 
         xml = table_row.xml
         self.assertEqual(xml, expected_xml)
