ab2a03683c5a26e927607cba118b7945c9608c96
Jason Ward <jason.ward@policystat.com>
2014-04-12 21:02:09 -0400

    refs #2: Added some failing tests.

--- html2docx/tests/test_paragraphs.py
+++ html2docx/tests/test_paragraphs.py
@@ -14,6 +14,22 @@ test_cases = [
         'Test multiple.',
         '<p>AAA</p><p>BBB</p>',
     ),
+    (
+        'Test bold.',
+        '<p><strong>AAA</strong></p>',
+    ),
+    (
+        'Test partial bold.',
+        '<p>A<strong>A</strong>A</p>',
+    ),
+    (
+        'Test em.',
+        '<p><em>AAA</em></p>',
+    ),
+    (
+        'Test partial em.',
+        '<p>A<em>A</em>A</p>',
+    ),
 ]
 
 
